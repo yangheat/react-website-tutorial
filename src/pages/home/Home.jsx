73853cff5bc7886fd64a516888729b4c7447a734
yangheat <yangheat@gamil.com>
2022-11-18 00:08:25 +0900

    programs component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import MainHeader from '../../components/mainHeader/MainHeader'
+import Programs from '../../components/programs/Programs'
+import Values from '../../components/Values'
 import './home.css'
 
 const home = () => {
   return (
     <>
     <MainHeader/>
+    <Programs/>
     </>
   )
 }
