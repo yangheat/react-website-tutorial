a0b7c50ae4cf281887a32ba34d2b05c0ccae9de7
yangheat <yangheat@gamil.com>
2022-11-23 00:21:03 +0900

    plans component

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,8 +1,40 @@
+import Header from '../../components/header/Header'
+import HeaderImage from '../../images/header_bg_5.jpg'
+import Card from '../../UI/Card'
+import {plans} from '../../data'
 import './plans.css'
 
 const Plans = () => {
   return (
-    <div>Plans</div>
+    <>
+    <Header title="Membership Plans" image={HeaderImage}>
+      Assumenda perspiciatis asperiores deserunt quisquam, sit iusto consectetur vero similique aliquam.
+    </Header>
+    <section className='plans'>
+      <div className="container plans-container"> 
+        {
+          plans.map(({id, name, desc, price, features}) => {
+            return (              
+              <Card key={id} className="plans-plan">
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>${price}</h1><h2>/mo</h2>
+                <h4>Feathres</h4>
+                {
+                  features.map(({feature, available}, index) => {
+                    return (
+                      <p key={index} className={!available && 'disabled'}>{feature}</p>
+                    )
+                  })
+                }
+                <button className='btn lg'>Choose Plan</button>
+              </Card>
+            )
+          })
+        }
+      </div>
+    </section>
+    </>
   )
 }
 
